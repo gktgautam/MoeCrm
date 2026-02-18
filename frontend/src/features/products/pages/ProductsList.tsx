fc7fbc0a42d37c7e214f41504b068e913824c55d
Gautam Kumar <gautamkumar@EWAs-MacBook-Pro.local>
2026-02-18 18:16:55 +0530

    xxxx

--- frontend/src/features/products/pages/ProductsList.tsx
+++ frontend/src/features/products/pages/ProductsList.tsx
@@ -1,14 +1,14 @@
 import PermissionGate from "@/core/rbac/PermissionGate";
-import Breadcrumb, { type BreadcrumbItem } from "@/components/ui/breadcrumb";
+// import Breadcrumb, { type BreadcrumbItem } from "@/components/ui/breadcrumb";
 
 import { ProductCreateForm } from "./ProductCreateForm";
 
 export default function ProductsList() {
     
-  const items: BreadcrumbItem[] = [
-    { label: "Home", path: "/" }, 
-    { label: "Product" } // last item has no link
-  ];
+  // const items: BreadcrumbItem[] = [
+  //   { label: "Home", path: "/" }, 
+  //   { label: "Product" } // last item has no link
+  // ];
 
 
     const handleCreate = async (payload: any) => {
@@ -24,7 +24,7 @@ export default function ProductsList() {
 
   return (
     <div className="space-y-3">
-      <Breadcrumb items={items} />
+      {/* <Breadcrumb items={items} /> */}
 
       <div className="flex justify-between items-center">
          <div className="text-lg font-semibold">Product Management</div>
